4bc1324c35a95e3e37e45897e49cb9d7cc3ffdae
mr.samani <mr.samani1368@gmail.com>
2025-12-23 12:51:52 +0330

    change context from properties to setting

--- projects/page-builder/src/components/block-properties/block-properties.component.ts
+++ projects/page-builder/src/components/block-properties/block-properties.component.ts
@@ -15,9 +15,6 @@ import { BackgroundControlComponent } from '../../controls/beckground-control/ba
 import { DisplayControlComponent } from '../../controls/display-control/display-control.component';
 import { TextCssControlComponent } from '../../controls/textcss-control/textcss-control.component';
 import { SizeControlComponent } from '../../controls/size-control/size-control.component';
-import { DynamicDataStructure } from '../../models/DynamicData';
-import { TextBindingComponent } from '../text-binding/text-binding.component';
-import { DynamicDataService } from '../../services/dynamic-data.service';
 import { ShadowControlComponent } from '../../controls/shadow-control/shadow-control.component';
 
 @Component({
@@ -34,7 +31,6 @@ import { ShadowControlComponent } from '../../controls/shadow-control/shadow-con
     DisplayControlComponent,
     TextCssControlComponent,
     SizeControlComponent,
-    TextBindingComponent,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
@@ -42,18 +38,11 @@ import { ShadowControlComponent } from '../../controls/shadow-control/shadow-con
 export class BlockPropertiesComponent extends BaseComponent implements OnInit {
   item?: PageItem;
 
-  parentCollection?: PageItem;
-  collectionDsList: DynamicDataStructure[] = [];
-
-  constructor(
-    injector: Injector,
-    private dynamicDataService: DynamicDataService,
-  ) {
+  constructor(injector: Injector) {
     super(injector);
     effect(() => {
       this.item = this.pageBuilder.activeEl();
       // console.log('updated properties', this.item);
-      this.checkParentIsCollection();
 
       this.chdRef.detectChanges();
     });
@@ -66,26 +55,5 @@ export class BlockPropertiesComponent extends BaseComponent implements OnInit {
 
   onChangeProperties() {
     if (this.item) this.pageBuilder.changedProperties(this.item);
-  }
-
-  checkParentIsCollection() {
-    this.parentCollection = this.parentCollectionItem(this.item);
-    if (this.parentCollection) {
-      const { id, skipCount, maxResultCount } = this.parentCollection.dataSource!;
-      let dsList = this.dynamicDataService.getCollectionData(id, skipCount, maxResultCount);
-      this.collectionDsList = dsList.length > 0 ? dsList[0] : [];
-    }
-  }
-
-  parentCollectionItem(item?: PageItem): PageItem | undefined {
-    if (!item) return undefined;
-    if (item.dataSource?.id) {
-      // if (item.template || item.customComponent?.componentKey == 'NgxPgHeroTable') {
-      return item;
-    }
-    if (item.parent) {
-      return this.parentCollectionItem(item.parent);
-    }
-    return undefined;
   }
 }
